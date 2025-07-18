a4ee85ef37190ce8040569320a7cf0f0fdac4a7e
mnvrohith <mnvrohith@gmail.com>
2025-07-18 11:33:18 +0530

    add react-hook-form for form handling and implement routing structure

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import {useForm} from 'react-hook-form' 
 
 const Login = () => { 
+  const { register, handleSubmit, formState: { errors } } = useForm(); 
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    // This effect runs once when the component mounts
+    // You can add any initialization logic here
+
+  }, [])
   return (
     <div>
-      Login
+
     </div>
   )
 }
